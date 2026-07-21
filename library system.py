c1715cd04a6427b09e5e5c0ac587d117d3baae72
Anita Reina <anitareina05@gmail.com>
2026-07-22 00:45:50 +0100

    started working on it

--- library system.py
+++ library system.py
@@ -5,10 +5,12 @@ books.remove("1984")
 books.sort()
 print(books)
 
-borrower_info = ({name}: {library_id}: {membership_date})
+
 name = input('Please input your name e.g "John Doe": ').title()
 library_id = input('Please enter your library ID e.g B1023: ')
-membership_date = int(input(
-    'Please enter the day you joined, e.g 2025-10-15: '))
-
+membership_date = input(
+    'Please enter the day you joined, e.g 2025-10-15: ')
+borrower_info = ({name}, {library_id}, {membership_date})
 print(len(borrower_info))
+borrower_info[0] = 'Riley Davis'
+print(borrower_info)
